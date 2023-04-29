09f9f05fd5456d9e564a9ba8fd2f705726296191
koidfas <koidfas1@gmail.com>
2023-04-29 10:22:29 -0300

    Added more patches

--- suckless/dwm-6.4/config.h
+++ suckless/dwm-6.4/config.h
@@ -27,9 +27,9 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class      instance    title       tags mask     iscentered   isfloating   monitor */
+	{ "Gimp",     NULL,       NULL,       0,            0,           1,           -1 },
+	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           0,           -1 },
 };
 
 /* layout(s) */
@@ -61,28 +61,28 @@ static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_Return,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,             		XK_space, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY,             		XK_w,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
-	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
+	{ MODKEY,                       XK_Return, spawn,     			{.v = dmenucmd } }, // Spawn dmenu
+	{ MODKEY,             					XK_space,  spawn,          	{.v = termcmd } }, // Spawn st
+	{ MODKEY,                       XK_b,      togglebar,      	{0} }, 
+	{ MODKEY,                       XK_j,      focusstack,     	{.i = +1 } },
+	{ MODKEY,                       XK_k,      focusstack,     	{.i = -1 } },
+	{ MODKEY,                       XK_i,      incnmaster,     	{.i = +1 } },
+	{ MODKEY,                       XK_d,      incnmaster,     	{.i = -1 } },
+	{ MODKEY,                       XK_h,      setmfact,       	{.f = -0.05} },
+	{ MODKEY,                       XK_l,      setmfact,       	{.f = +0.05} },
+	{ MODKEY,                       XK_Tab,    view,           	{0} },
+	{ MODKEY,             					XK_w,      killclient,     	{0} },
+	{ MODKEY,                       XK_t,      setlayout,      	{.v = &layouts[0]} },
+	{ MODKEY,                       XK_f,      setlayout,      	{.v = &layouts[1]} },
+	{ MODKEY,                       XK_0,      view,           	{.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,      tag,            	{.ui = ~0 } },
+	{ MODKEY,                       XK_comma,  focusmon,       	{.i = -1 } },
+	{ MODKEY,                       XK_period, focusmon,       	{.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         	{.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_period, tagmon,         	{.i = +1 } },
+	{ MODKEY,                       XK_minus,  setgaps,        	{.i = -1 } },
+	{ MODKEY,                       XK_equal,  setgaps,        	{.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        	{.i = 0  } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
