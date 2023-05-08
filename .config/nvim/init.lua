ded79c47cfd5cbf248742d652f7f2577015aff5e
koidfas <leonardogauss615@gmail.com>
2023-05-07 21:04:59 -0300

    oops

--- .config/nvim/init.lua
+++
@@ -1,6 +0,0 @@
-require('config')
-require('lazy-nvim')
-
--- Colorscheme
-local colorscheme = 'nord'
-local colorscheme_set, _ = pcall(vim.cmd, 'colorscheme ' .. colorscheme)
--- .config/nvim/lua/plugins/lualine.lua
+++
@@ -1,7 +0,0 @@
-return {
-	'nvim-lualine/lualine.nvim',
-	config = true,
-	dependencies = {
-		{'kyazdani42/nvim-web-devicons'},
-	},
-}
