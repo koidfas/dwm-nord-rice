ee2a11c30cbe498bc3d85e2bdbe9654910b9faed
koidfas <koidfas1@gmail.com>
2023-07-11 06:31:27 -0300

    HOW THE FUCK I FORGOT TO START THE GNOME POLKIT????

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -15,6 +15,9 @@ nitrogen --restore
 # Start slstatus for bar
 slstatus &
 
+# Polkit
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+
 # Notification 
 /usr/lib/xfce4/notifyd/xfce4-notifyd & 
 
